8b73a30dc6274f42ead45e93ef4ee4fe59df65f3
tendryAxel <hei.tendry@gmail.com>
2026-08-17 11:39:27 +0300

    chore: display anime image

--- src/anime_gui/anime_info_api/components/search_list.py
+++ src/anime_gui/anime_info_api/components/search_list.py
@@ -1,10 +1,16 @@
+import requests
 from toga.style import Pack
-from toga import Box, Label
+from toga import Box, Label, Image, ImageView
 from kitsu_extended import Anime
-from toga.style.pack import COLUMN
+from toga.style.pack import COLUMN, ROW
 
 
 def anime_in_search_result(anime: Anime) -> Box:
+    # TODO: make image loading async
+    image_url = anime.poster_image("tiny")
+    if image_url is not None:
+        image_data = requests.get(image_url).content
+
     return Box(
         children=[
             Label(
@@ -15,18 +21,46 @@ def anime_in_search_result(anime: Anime) -> Box:
                     margin_bottom=5,
                 ),
             ),
-            Label(
-                f"⭐ {anime.average_rating}  •  "
-                f"{anime.subtype}  •  "
-                f"{anime.episode_count or '?'} episodes  •  "
-                f"{anime.status}",
+
+            Box(
+                children=[
+                    ImageView(
+                        Image(image_data),
+                        style=Pack(
+                            width=180,
+                            height=240,
+                            margin_right=15,
+                        ),
+                    ),
+
+                    # Anime information
+                    Box(
+                        children=[
+                            Label(
+                                f"⭐ {anime.average_rating or '?'}  •  "
+                                f"{anime.subtype or '?'}  •  "
+                                f"{anime.episode_count or '?'} episodes  •  "
+                                f"{anime.status or '?'}",
+                                style=Pack(
+                                    margin_bottom=8,
+                                ),
+                            ),
+
+                            Label(
+                                anime.synopsis or "No synopsis available.",
+                                style=Pack(
+                                    flex=1,
+                                ),
+                            ),
+                        ],
+                        style=Pack(
+                            direction=COLUMN,
+                            flex=1,
+                        ),
+                    ),
+                ],
                 style=Pack(
-                    margin_bottom=8,
-                ),
-            ),
-            Label(
-                anime.synopsis or "No synopsis available.",
-                style=Pack(
+                    direction=ROW,
                     flex=1,
                 ),
             ),
