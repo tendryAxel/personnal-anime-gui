2c2f32840c0d4e242efdd9f68950f77bd6381837
tendryAxel <hei.tendry@gmail.com>
2026-08-17 09:40:20 +0300

    feat: search anime by name

--- src/anime_gui/anime_info_api/main.py
+++ src/anime_gui/anime_info_api/main.py
@@ -1,13 +1,31 @@
+import dataclasses
 from types import CoroutineType
 from typing import TypeIs, Callable, Any
 import kitsu_extended as kitsu
+import dotenv
+
+dotenv.load_dotenv()
 
 
-def isAnime_validation(to_valid) -> TypeIs[kitsu.Anime]:
+def isAnime(to_valid) -> TypeIs[kitsu.Anime]:
     if isinstance(to_valid, kitsu.Anime):
         return True
     
+    return False
+
+
+def isAnime_validation(to_valid) -> TypeIs[kitsu.Anime]:
+    if isAnime(to_valid):
+        return True
+    
     raise TypeError(f"Need parsing implementation of {type(to_valid) = }, that contains: {to_valid}")
+
+
+def isAnime_list_validation(to_valid) -> TypeIs[list[kitsu.Anime]]:
+    for element in to_valid:
+        isAnime_validation(element)
+    
+    return True
 
 async def _api_request[T](request: Callable[[kitsu.Client], CoroutineType[Any, Any, T]]) -> T:
     client = kitsu.Client()
@@ -16,3 +34,23 @@ async def _api_request[T](request: Callable[[kitsu.Client], CoroutineType[Any, A
         return await request(client)
     finally:
         await client.close()
+
+@dataclasses.dataclass
+class PageParam:
+    page_number: int
+    page_size: int
+
+    def __post_init__(self):
+        if self.page_number < 0:
+            raise ValueError(f"Page number must be positive number, not {self.page_number}")
+        
+        if self.page_size < 0:
+            raise ValueError(f"Page size must be positive number, not {self.page_size}")
+    
+    @property
+    def limit(self):
+        return self.page_size
+    
+    @property
+    def offset(self):
+        return self.page_size * self.page_number
--- tests/test_anime_info_api.py
+++ tests/test_anime_info_api.py
@@ -1,9 +1,16 @@
 from anime_gui.anime_info_api import anime
 
 
-def test_get_by_id():
+def test_get_anime_by_id():
     anime_id = 21
     result = anime.get_by_id(anime_id)
 
     assert result.id == str(anime_id)
     assert result.title == "Neon Genesis Evangelion"
+
+def test_find_anime_by_name():
+    result = anime.find_by_name("recoil")
+
+    assert isinstance(result, list)
+    assert len(result) >= 3
+    assert result[0].title == "Lycoris Recoil"
