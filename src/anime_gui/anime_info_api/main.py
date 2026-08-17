79a0d068588f160bd4e53e252a85f157310a66f0
tendryAxel <hei.tendry@gmail.com>
2026-08-17 08:38:07 +0300

    chore: anime validation

--- src/anime_gui/anime_info_api/main.py
+++ src/anime_gui/anime_info_api/main.py
@@ -1,20 +1,25 @@
+from typing import TypeIs
 import asyncio
 import kitsu_extended as kitsu
 
 
-async def async_get_by_id(anime_id: int) -> kitsu.Anime | dict:
-    client = kitsu.Client()
-
-    try:
-        return await client.get_anime(anime_id)
-    finally:
-        await client.close()
-
+def isAnime_validation(to_valid) -> TypeIs[kitsu.Anime]:
+    if isinstance(to_valid, kitsu.Anime):
+        return True
+    
+    raise TypeError(f"Need parsing implementation of {type(to_valid) = }, that contains: {to_valid}")
 
 def get_by_id(anime_id: int) -> kitsu.Anime:
+    async def async_get_by_id(anime_id: int) -> kitsu.Anime | dict:
+        client = kitsu.Client()
+
+        try:
+            return await client.get_anime(anime_id)
+        finally:
+            await client.close()
+            
     result = asyncio.run(async_get_by_id(anime_id))
 
-    if isinstance(result, kitsu.Anime):
-        return result
-    
-    raise Exception(f"Need parsing implementation of {type(result) = }, that contains\n{result}")
+    assert isAnime_validation(result)
+
+    return result
